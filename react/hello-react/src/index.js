df84286ae0c953193134b33e654bf81b96349bd2
Ali Susanto <alisusanto@smu.edu.sg>
2022-02-12 14:33:36 +0800

    useState React

--- react/hello-react/src/index.js
+++ react/hello-react/src/index.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
 let city = { name: "singapore", country: "singapore" };
 
+function Lake({name}){
+  return <h1>Lake {name}!</h1>
+}
+
+function SkiResport(){
+  return <h1>Ski Report!!</h1>
+}
+
+function App(props){
+  const [status, setStatus] = useState("Open");
+  return (
+    <>
+      <h1>Status:{status}</h1>
+      <button onClick={() => }>Open</button>
+    </>
+  )
+}
+
 ReactDOM.render(
-    <h1 id="heading" className='cooltext'>{city.name} is in {city.country}</h1>,
+    <App name="react" />,
     document.getElementById('root')
   );
+
+
+const snacks = ["Popcorn", "Chips", "Cassava"];
